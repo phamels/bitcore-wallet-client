376508c279cc00f8394c62789ee70b91c0d0490a
Pieter Hamels <pieter@hamels.be>
2018-09-13 13:18:07 +0200

    Nix Only

--- test/client.js
+++ test/client.js
@@ -16,11 +16,9 @@ var tingodb = require('tingodb')({
 
 var log = require('../lib/log');
 
-var Bitcore = require('bitcore-lib');
+var Bitcore = require('nix-bitcore-lib');
 var Bitcore_ = {
-  btc: Bitcore,
-  bch: require('bitcore-lib-cash'),
-  nix: require('nix-bitcore-lib'),
+  nix: Bitcore,
 };
 
 
